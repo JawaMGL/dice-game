337f72fd088dd9fac369932a5a8e2f11a1364a3d
JavkhlanE <JavkhlanEnkhjin@gmail.com>
2020-02-02 19:00:00 +0800

    updated app.js

--- dice-game/app.js
+++ dice-game/app.js
@@ -10,12 +10,36 @@ var scores = [0, 0];
 var tempScore = 0;
 
 // dice
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceDom = document.querySelector(".dice");
 
 // Preparation to starting
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").innerHTML = 0;
-document.querySelector(".dice").style.display = "none";
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-console.log(dice);
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+diceDom.style.display = "none";
+
+// rollDice event
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+
+  if (diceNumber !== 1) {
+    tempScore = tempScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = tempScore;
+  } else {
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    tempScore = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceDom.style.display = "none";
+  }
+});
+
+var btnHold = document.querySelector(".btn-hold");
+
+// diceNumber not equal to 0 add number to active players current score.
