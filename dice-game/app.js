eb77b962423833fa8f5ca81a6565b2969effe0ec
JavkhlanE <JavkhlanEnkhjin@gmail.com>
2020-02-02 21:20:57 +0800

    done

--- dice-game/app.js
+++ dice-game/app.js
@@ -1,55 +1,70 @@
 // player turn
 // 1st player 0
 // 2nd player 1
-var activePlayer = 0;
-
-// players current score
-var scores = [0, 0];
-
-// players temp score
-var tempScore = 0;
-
-// dice
+var scores;
+var isGameOver;
+var activePlayer;
+var tempScore;
 var diceDom = document.querySelector(".dice");
-
-// Preparation to starting
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-
-diceDom.style.display = "none";
+// players current score
+initGame();
+function initGame() {
+  isGameOver = false;
+  scores = [0, 0];
+  // players temp score
+  activePlayer = 0;
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  tempScore = 0;
+  // Preparation to starting
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+  diceDom.style.display = "none";
+}
 
 // rollDice event
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  diceDom.style.display = "block";
-  diceDom.src = "dice-" + diceNumber + ".png";
+  if (isGameOver === false) {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
 
-  if (diceNumber !== 1) {
-    tempScore = tempScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = tempScore;
-  } else {
-    switchToNextPlayer();
+    if (diceNumber !== 1) {
+      tempScore = tempScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = tempScore;
+    } else {
+      switchToNextPlayer();
+    }
   }
 });
 
 var btnHold = document.querySelector(".btn-hold");
 btnHold.addEventListener("click", function() {
-  scores[activePlayer] = scores[activePlayer] + tempScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 10) {
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    switchToNextPlayer();
+  if (isGameOver === false) {
+    scores[activePlayer] = scores[activePlayer] + tempScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 10) {
+      isGameOver = true;
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      switchToNextPlayer();
+    }
   }
 });
 
@@ -63,5 +78,5 @@ function switchToNextPlayer() {
   diceDom.style.display = "none";
 }
 document.querySelector(".btn-new").addEventListener("click", function() {
-  alert("fuku");
+  initGame();
 });
